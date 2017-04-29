bf2d94d4d4345da98b652ff7f93928f90c37a2bd
ezavile <ezavile@gmail.com>
2017-04-29 03:08:12 -0500

    add server and dist folder

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Create our app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else { 
+    next();
+  }
+});
+
+app.use(express.static('dist'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
